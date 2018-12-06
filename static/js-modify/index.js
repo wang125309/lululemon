3b7bdc53163524d0b6e754c53c52f1c77e74cb24
wang_125309 <wang_125309@foxmail.com>
2018-12-06 18:29:11 +0800

    fix

--- static/js-modify/index.js
+++ static/js-modify/index.js
@@ -1,15 +1,24 @@
 import Swiper from 'swiper'
 
-window.onload = () => {
-    new Swiper('.swiper-container', {
-        direction:'vertical',
-        speed:500,
-        onInit () {
-        },
-        onSlideChangeEnd(swiper) {
-            if (swiper.activeIndex === 0) {
+let imageLoad = () => {
+  let imgArrays = ['page1-bg.png', 'page1-text1-bg.png', 'logo.png']
+  let url = '/static/img/'
+  img
+  new Promise((resolve, reject) => {
 
-            }
-        }
-    })
+  })
+}
+
+window.onload = () => {
+  new Swiper('.swiper-container', {
+    direction:'vertical',
+    speed:500,
+    onInit () {
+    },
+    onSlideChangeEnd(swiper) {
+      if (swiper.activeIndex === 0) {
+
+      }
+    }
+  })
 }
